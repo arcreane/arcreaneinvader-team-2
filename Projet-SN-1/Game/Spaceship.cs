9e6b55b95e5693232a4673b18cbb554242062914
N0reply117 <chablozossyane@gmail.com>
2020-10-15 20:40:20 +0200

    Ajout de modifications dans Bonus et Spaceship

--- Projet-SN-1/Game/Spaceship.cs
+++ Projet-SN-1/Game/Spaceship.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projet_SN_1.Jeu
+namespace Projet_SN_1
 {
-    class Vaisseau
+    public class Spaceship : Sprite
     {
+        public bool bIsDead = false;
+        public string sSkin = "!_oZo_!";
+
+        public Projectile m_Projectile;
+
+        public override void Move(Sprite p_SpriteToMove) { }
+        public override void Shoot(Projectile p_ProjectileToShoot) { }
+        public override void ApplyDamagesToEnnemy(Sprite p_SpriteToApplyDamage) { }
+        public override void ReceiveDamage(Sprite p_SpriteToApplyDamage) { }
     }
 }
